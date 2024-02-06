f5b560321e11215e2db437f5fc87a6ec4f9e3ea1
すみそ <sumi@wattai.jp>
2024-02-06 13:45:26 +0900

    update: 環境変数

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      microCmsApiServiceUrl: process.env.MICROCMS_API_SERVICE_URL,
+      microCmsApiKey: process.env.MICROCMS_API_KEY,
+    },
+  },
+});
