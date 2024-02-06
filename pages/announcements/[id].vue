35195ab37431b72c5283771bb3690118f3a21740
すみそ <sumi@wattai.jp>
2024-02-06 15:55:32 +0900

    update: お知らせページ実装

---
+++ pages/announcements/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Announcement</h1>
+    {{ announcement }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig().public;
+const route = useRoute();
+
+const announcement = await $fetch(`/news/${route.params.id}`, {
+  method: "GET",
+  headers: { "X-MICROCMS-API-KEY": config.microCmsApiKey },
+  baseURL: config.microCmsApiServiceUrl,
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p></p>
+    <h2>Event</h2>
+    <ul>
+      <li v-for="event of events.contents" :key="event.id">
+        <NuxtLink :to="`/events/${event.id}`"> {{ event.name }} </NuxtLink>
+      </li>
+    </ul>
+    <h2>News</h2>
+    <ul>
+      <li v-for="announcement of announcements.contents" :key="announcement.id">
+        <NuxtLink :to="`/announcements/${announcement.id}`"> {{ announcement.title }} </NuxtLink>
+      </li>
+    </ul>
+    {{ news }}
   </div>
 </template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig().public;
+
+const events = await $fetch("/api/events");
+const announcements = await $fetch("/news", {
+  method: "GET",
+  headers: { "X-MICROCMS-API-KEY": config.microCmsApiKey },
+  baseURL: config.microCmsApiServiceUrl,
+});
+</script>
